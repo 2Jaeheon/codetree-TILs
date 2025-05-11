2f84ff9470fe6825fee0dec3ea8578b98d4fc515
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2025-05-11 23:14:14 +0900

    Title: 더 긴 문자열, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250511/더 긴 문자열/longer-string.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        String str2 = sc.next();
+
+        if(str.length() > str2.length()){
+            System.out.println(str + " " + str.length());
+        } else if(str.length() < str2.length()){
+            System.out.println(str2 + " " + str2.length());
+        } else {
+            System.out.println("same");
+        }
+    }
+}
